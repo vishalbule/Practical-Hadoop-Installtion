89a72655a60e88641d542903a94c84e2150658c7
Vishal Bule <vishalbule24@gmail.com>
2018-08-27 08:51:10 +0530

    Update SqoopSetup.sh

--- SqoopSetup.sh
+++ SqoopSetup.sh
@@ -7,7 +7,22 @@ Please note that sqoop version can be different like 1.x.x.
 Go to Downloads directory and download latest tar.gz file for sqoop.
 https://hbase.apache.org
 
+Extract .tar.gz file
+-------------------------
+sudo tar -zxf sqoop-1.4.7.bin__hadoop-2.6.0.tar.gz
+
 How to change the owner and group
 -----------------------------------
-sudo chown -R  vishal:root hbase-1.4.6
+sudo chown -R  vishal:root sqoop-1.4.7
+
+Add below lines at the end of profile
+---------------------------------------
+# Set SQOOP_HOME
+export SQOOP_HOME="/usr/local/sqoop-1.4.7/"
+PATH=$PATH:$SQOOP_HOME/bin
+export PATH
+
+if you are planning to import data from MySQL database then you need to copy related jar file in Sqoop installation directory.
+-----------------------------------------------------------------------------------------------------------------------------------
+cp /usr/share/java/mysql-connector-java.jar $SQOOP_HOME/lib
 
