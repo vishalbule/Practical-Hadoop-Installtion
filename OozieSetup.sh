cb0a137a36027e9c92c3a6056d16386ff5f3c773
Vishal Bule <vishalbule24@gmail.com>
2018-08-27 08:30:49 +0530

    Create OozieSetup.sh

---
+++ OozieSetup.sh
@@ -0,0 +1,8 @@
+Install Oozie
+--------------------------------
+You should get new version if there are new versions available on apache site.
+https://oozie.apache.org/
+
+How to change the owner and group
+-----------------------------------
+sudo chown -R  vishal:root oozie-5.0.0
