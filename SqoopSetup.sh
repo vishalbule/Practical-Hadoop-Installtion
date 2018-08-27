1f8296942f84821a1a3f1550c25a249a3a6846ec
Vishal Bule <vishalbule24@gmail.com>
2018-08-27 08:41:47 +0530

    Create SqoopSetup.sh

---
+++ SqoopSetup.sh
@@ -0,0 +1,13 @@
+Install Sqoop (version 1.4.x)
+-------------------------------
+Ensure that you download version that matches with your installed hadoop major version.
+If your hadoop version is 2.x.x then download sqoop version that ends with 2.x.x.
+For example if your hadoop version is 2.8.1 then download sqoop-1.4.6.bin__hadoop-2.0.4-alpha.tar.gz.
+Please note that sqoop version can be different like 1.x.x.
+Go to Downloads directory and download latest tar.gz file for sqoop.
+https://hbase.apache.org
+
+How to change the owner and group
+-----------------------------------
+sudo chown -R  vishal:root hbase-1.4.6
+
